78095f98447a6cc95d835183577f1ae3035e54d5
Vrishali <65127767+Vrishali34@users.noreply.github.com>
2024-11-08 22:22:59 +0530

    Delete 1829_ directory

--- 1829_/1829_maximum_xor_for_each_query.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    vector<int> getMaximumXor(vector<int>& nums, int maximumBit) {
-        int mask=(1<<maximumBit)-1;
-        for(int i=0, x=0; i<nums.size(); i++) x^=nums[i], nums[i]=x^mask;
-        reverse(nums.begin(), nums.end());
-        return nums;
-    }
-};
