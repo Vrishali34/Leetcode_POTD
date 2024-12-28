147ec939d304a6df7bef1c52bcbd51534e0c1f3f
Vrishali <65127767+Vrishali34@users.noreply.github.com>
2024-12-28 23:36:29 +0530

    Create 689_maximum_sum_of_3_non_overlapping_subarrays.cpp

---
+++ 689_maximum_sum_of_3_non_overlapping_subarrays.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> maxSumOfThreeSubarrays(vector<int>& x, int y) {
+        int a = x.size(), b = 0, c = 0, d = 0, e = 0, f = 0, g = 0, h = 0, i = 0, j = y, k = 2 * y;
+        vector<int> z = {0, y, 2 * y};
+        for (int p = 0; p < y; p++) b += x[p], c += x[p + y], d += x[p + 2 * y];
+        e = b, f = b + c, g = b + c + d;
+        for (int m = 0; m <= a - 3 * y; m++) {
+            if (m > 0) b += x[m + y - 1] - x[m - 1], c += x[m + 2 * y - 1] - x[m + y - 1], d += x[m + 3 * y - 1] - x[m + 2 * y - 1];
+            if (b > e) e = b, h = m;
+            if (e + c > f) f = e + c, i = h, j = m + y;
+            if (f + d > g) g = f + d, z = {i, j, m + 2 * y};
+        }
+        return z;
+    }
+};
