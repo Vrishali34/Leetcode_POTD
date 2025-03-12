22f2b3858363542c8a4e0db70c747ea2b7593940
Vrishali <65127767+Vrishali34@users.noreply.github.com>
2025-03-12 20:30:19 +0530

    Create 2529_maximum_count_of_positive_integer_and_negative_integer.java

---
+++ 2529_maximum_count_of_positive_integer_and_negative_integer.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+class Solution {
+    public int maximumCount(int[] nums) {
+        return Math.max((int) Arrays.stream(nums).filter(x -> x > 0).count(),
+                        (int) Arrays.stream(nums).filter(x -> x < 0).count());
+    }
+}
