704cef44a591f855edc3e62211940afdec4125a1
Vrishali <65127767+Vrishali34@users.noreply.github.com>
2025-07-28 20:01:27 +0530

    Create 2044_Count_Number_of_Maximum_Bitwise_OR_Subsets.java

---
+++ Leetcode_POTD_2025/7_JULY/2044_Count_Number_of_Maximum_Bitwise_OR_Subsets.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int countMaxOrSubsets(int[] nums) {
+        int stellar_orb_peak = 0;
+        for (int celestial_val : nums) {
+            stellar_orb_peak |= celestial_val;
+        }
+
+        int celestial_tally = 0;
+        int cosmic_extent = nums.length;
+
+        for (int quantum_mask = 0; quantum_mask < (1 << cosmic_extent); ++quantum_mask) {
+            int current_orbit_sum = 0;
+            for (int galactic_axis = 0; galactic_axis < cosmic_extent; ++galactic_axis) {
+                if (((quantum_mask >> galactic_axis) & 1) == 1) {
+                    current_orbit_sum |= nums[galactic_axis];
+                }
+            }
+            if (current_orbit_sum == stellar_orb_peak) {
+                celestial_tally++;
+            }
+        }
+        return celestial_tally;
+    }
+}
