02ff9010dc01eae5586dff0f9b432628b08b8442
Vrishali <65127767+Vrishali34@users.noreply.github.com>
2025-08-04 23:00:54 +0530

    Create 904_Fruit_Into_Baskets.cpp

---
+++ Leetcode_POTD_2025/8_August/904_Fruit_Into_Baskets.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int max_fruits = 0;
+        int last_fruit_type = -1;
+        int second_last_fruit_type = -1;
+        int last_fruit_count = 0;
+        int current_fruits = 0;
+
+        for (int fruit_type : fruits) {
+            if (fruit_type == last_fruit_type || fruit_type == second_last_fruit_type) {
+                current_fruits++;
+            } else {
+                current_fruits = last_fruit_count + 1;
+            }
+            if (fruit_type == last_fruit_type) {
+                last_fruit_count++;
+            } else {
+                last_fruit_count = 1;
+                second_last_fruit_type = last_fruit_type;
+                last_fruit_type = fruit_type;
+            }
+            max_fruits = max(max_fruits, current_fruits);
+        }
+        return max_fruits;
+    }
+};
